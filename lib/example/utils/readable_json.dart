2cad2696a8fc24e7ffeb0165f0c9d3a4fe6d95de
Syméon ROUGEVIN <symeon@oclocher.fr>
2024-06-09 17:19:32 +0200

    Improved readableJson

--- lib/example/utils/readable_json.dart
+++ lib/example/utils/readable_json.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:package_atomic_design/package_atomic_design.dart';
@@ -18,19 +20,25 @@ void showJsonDialog(BuildContext context) {
 String readableJson(Map<String, dynamic> json, {int indentStep = 4}) {
   final buffer = StringBuffer();
   var indent = 0;
+  var insideString = false;
+  var lastChar = '';
 
-  for (final char in json.toString().characters) {
-    if (char == '}' || char == ']') {
+  for (final char in jsonEncode(json).characters) {
+    if (!insideString && (char == '}' || char == ']')) {
       indent -= indentStep;
       buffer.write('\n${' ' * indent}');
     }
     buffer.write(char);
-    if (char == '{' || char == '[') {
-      indent += indentStep;
-      buffer.write('\n${' ' * indent}');
-    } else if (char == ',') {
-      buffer.write('\n${' ' * indent}');
+    if (char == '"' && lastChar != r'\') insideString = !insideString;
+    if (!insideString) {
+      if (char == '{' || char == '[') {
+        indent += indentStep;
+        buffer.write('\n${' ' * indent}');
+      } else if (char == ',') {
+        buffer.write('\n${' ' * indent}');
+      }
     }
+    lastChar = char;
   }
 
   return buffer.toString();
