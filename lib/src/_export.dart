1637447dc000ba1d405a337cec72391f56f799af
SymAir <symeon@oclocher.fr>
2024-06-17 22:56:31 +0200

    rename

--- lib/src/_export.dart
+++ lib/src/_export.dart
@@ -1,5 +1,5 @@
 export 'cubit/_export.dart';
-export 'field_builder/_export.dart';
+export 'builder/_export.dart';
 export 'l10n/_export.dart';
 export 'model/_export.dart';
 export 'theme/_export.dart';
