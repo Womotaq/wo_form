221c9c4e8c8e50905b37d5371e75f3ec665b084d
SymAir <symeon@oclocher.fr>
2024-07-08 23:55:53 +0200

    Added ValueBuilderNodes in FormCreatorPage's dynamic node

--- lib/example/form_creator/num_input_node.dart
+++ lib/example/form_creator/num_input_node.dart
@@ -1,66 +1,75 @@
 import 'package:wo_form/wo_form.dart';
 
-InputsNode createNumInputNode() => const InputsNode(
-      id: 'numInput',
-      uiSettings: InputsNodeUiSettings(
-        labelText: 'Saisie de nombre',
-        inputsVisibility: InputsVisibility.whenAsked,
-      ),
-      inputs: [
-        StringInput(
-          id: 'id',
-          isRequired: true,
-          uiSettings: StringInputUiSettings(
-            labelText: 'Clef json',
-            autofocus: true,
+WoFormElementMixin createNumInputNode() => ValueBuilderNode(
+      id: 'labelText-builder',
+      inputPath: './numInput/uiSettings/labelText',
+      builder: (context, value) {
+        value as String?;
+        return InputsNode(
+          id: 'numInput',
+          exportSettings: const ExportSettings(
+            metadata: {
+              'runtimeType': 'num',
+            },
           ),
-        ),
-        NumInput(
-          id: 'initialValue',
-          uiSettings: NumInputUiSettings(
-            labelText: 'Valeur par défaut',
-          ),
-        ),
-        BooleanInput(
-          id: 'isRequired',
-          uiSettings: BooleanInputUiSettings(
-            labelText: 'Doit être renseigné',
-            helperText: 'Un champ vide ne sera pas accepté. 0 sera accepté.',
-          ),
-        ),
-        NumInput(
-          id: 'minBound',
-          initialValue: 0,
-          isRequired: true,
-          uiSettings: NumInputUiSettings(
-            labelText: 'Valeur minimale',
-          ),
-        ),
-        NumInput(
-          id: 'maxBound',
-          uiSettings: NumInputUiSettings(
-            labelText: 'Valeur maximale',
-          ),
-        ),
-        InputsNode(
-          id: 'uiSettings',
           uiSettings: InputsNodeUiSettings(
-            labelText: 'Interface',
+            labelText: value ?? 'Saisie de nombre',
+            helperText: value == null ? null : 'Saisie de nombre',
             inputsVisibility: InputsVisibility.whenAsked,
           ),
           inputs: [
-            StringInput(
-              id: 'labelText',
+            const StringInput(
+              id: 'id',
+              isRequired: true,
               uiSettings: StringInputUiSettings(
-                labelText: 'Titre',
+                labelText: 'Clef json',
+                autofocus: true,
               ),
             ),
+            const NumInput(
+              id: 'initialValue',
+              uiSettings: NumInputUiSettings(
+                labelText: 'Valeur par défaut',
+              ),
+            ),
+            const BooleanInput(
+              id: 'isRequired',
+              uiSettings: BooleanInputUiSettings(
+                labelText: 'Doit être renseigné',
+                helperText:
+                    'Un champ vide ne sera pas accepté. 0 sera accepté.',
+              ),
+            ),
+            const NumInput(
+              id: 'minBound',
+              initialValue: 0,
+              isRequired: true,
+              uiSettings: NumInputUiSettings(
+                labelText: 'Valeur minimale',
+              ),
+            ),
+            const NumInput(
+              id: 'maxBound',
+              uiSettings: NumInputUiSettings(
+                labelText: 'Valeur maximale',
+              ),
+            ),
+            const InputsNode(
+              id: 'uiSettings',
+              uiSettings: InputsNodeUiSettings(
+                labelText: 'Interface',
+                inputsVisibility: InputsVisibility.whenAsked,
+              ),
+              inputs: [
+                StringInput(
+                  id: 'labelText',
+                  uiSettings: StringInputUiSettings(
+                    labelText: 'Titre',
+                  ),
+                ),
+              ],
+            ),
           ],
-        ),
-      ],
-      exportSettings: ExportSettings(
-        metadata: {
-          'runtimeType': 'num',
-        },
-      ),
+        );
+      },
     );
--- lib/example/form_creator/select_string_input_node.dart
+++ lib/example/form_creator/select_string_input_node.dart
@@ -1,117 +1,126 @@
 import 'package:flutter/material.dart';
 import 'package:wo_form/wo_form.dart';
 
-InputsNode createSelectStringInputNode() => InputsNode(
-      id: 'selectStringInput',
-      uiSettings: const InputsNodeUiSettings(
-        labelText: 'Choix de texte',
-        inputsVisibility: InputsVisibility.whenAsked,
-      ),
-      inputs: [
-        const StringInput(
-          id: 'id',
-          isRequired: true,
-          uiSettings: StringInputUiSettings(
-            labelText: 'Clef json',
-            autofocus: true,
+WoFormElementMixin createSelectStringInputNode() => ValueBuilderNode(
+      id: 'labelText-builder',
+      inputPath: './selectStringInput/uiSettings/labelText',
+      builder: (context, value) {
+        value as String?;
+        return InputsNode(
+          id: 'selectStringInput',
+          exportSettings: const ExportSettings(
+            metadata: {
+              'runtimeType': 'selectString',
+            },
           ),
-        ),
-        const NumInput(
-          id: 'maxCount',
-          initialValue: 1,
-          minBound: 1,
-          isRequired: true,
-          uiSettings: NumInputUiSettings(
-            labelText: 'Nombre maximum de réponses',
-          ),
-        ),
-        const NumInput(
-          id: 'minCount',
-          initialValue: 0,
-          isRequired: true,
-          uiSettings: NumInputUiSettings(
-            labelText: 'Nombre minimum de réponses',
-          ),
-        ),
-        InputsNode(
-          id: 'uiSettings',
-          uiSettings: const InputsNodeUiSettings(
-            inputsVisibility: InputsVisibility.always,
+          uiSettings: InputsNodeUiSettings(
+            labelText: value ?? 'Choix de texte',
+            helperText: value == null ? null : 'Choix de texte',
+            inputsVisibility: InputsVisibility.whenAsked,
           ),
           inputs: [
-            InputsNode(
-              id: 'uiSettings-More',
-              exportSettings: const ExportSettings(
-                type: ExportType.mergeWithParent,
+            const StringInput(
+              id: 'id',
+              isRequired: true,
+              uiSettings: StringInputUiSettings(
+                labelText: 'Clef json',
+                autofocus: true,
               ),
+            ),
+            const NumInput(
+              id: 'maxCount',
+              initialValue: 1,
+              minBound: 1,
+              isRequired: true,
+              uiSettings: NumInputUiSettings(
+                labelText: 'Nombre maximum de réponses',
+              ),
+            ),
+            const NumInput(
+              id: 'minCount',
+              initialValue: 0,
+              isRequired: true,
+              uiSettings: NumInputUiSettings(
+                labelText: 'Nombre minimum de réponses',
+              ),
+            ),
+            InputsNode(
+              id: 'uiSettings',
               uiSettings: const InputsNodeUiSettings(
-                labelText: 'Interface',
-                inputsVisibility: InputsVisibility.whenAsked,
+                inputsVisibility: InputsVisibility.always,
               ),
               inputs: [
-                const StringInput(
-                  id: 'helperText',
-                  uiSettings: StringInputUiSettings(
-                    labelText: 'Sous-titre',
+                InputsNode(
+                  id: 'uiSettings-More',
+                  exportSettings: const ExportSettings(
+                    type: ExportType.mergeWithParent,
                   ),
-                ),
-                const StringInput(
-                  id: 'hintText',
-                  uiSettings: StringInputUiSettings(
-                    labelText: 'Affichage en cas de valeur nulle',
+                  uiSettings: const InputsNodeUiSettings(
+                    labelText: 'Interface',
+                    inputsVisibility: InputsVisibility.whenAsked,
                   ),
-                ),
-                SelectInput<InputsVisibility?>(
-                  id: 'inputsVisibility',
-                  initialValues: [InputsVisibility.always],
-                  availibleValues: InputsVisibility.values,
-                  minCount: 1,
-                  maxCount: 1,
-                  uiSettings: SelectInputUiSettings(
-                    labelText: 'Visibilité des options',
-                    valueBuilder: (value) => Text(
-                      switch (value) {
-                        null => "Sélectionnez un type d'affichage",
-                        InputsVisibility.whenAsked =>
-                          'Cliquer pour voir les options',
-                        InputsVisibility.always => 'Toujours voir les options',
-                      },
+                  inputs: [
+                    const StringInput(
+                      id: 'helperText',
+                      uiSettings: StringInputUiSettings(
+                        labelText: 'Sous-titre',
+                      ),
                     ),
+                    const StringInput(
+                      id: 'hintText',
+                      uiSettings: StringInputUiSettings(
+                        labelText: 'Affichage en cas de valeur nulle',
+                      ),
+                    ),
+                    SelectInput<InputsVisibility?>(
+                      id: 'inputsVisibility',
+                      initialValues: [InputsVisibility.always],
+                      availibleValues: InputsVisibility.values,
+                      minCount: 1,
+                      maxCount: 1,
+                      uiSettings: SelectInputUiSettings(
+                        labelText: 'Visibilité des options',
+                        valueBuilder: (value) => Text(
+                          switch (value) {
+                            null => "Sélectionnez un type d'affichage",
+                            InputsVisibility.whenAsked =>
+                              'Cliquer pour voir les options',
+                            InputsVisibility.always =>
+                              'Toujours voir les options',
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const StringInput(
+                  id: 'labelText',
+                  uiSettings: StringInputUiSettings(
+                    labelText: 'Titre',
                   ),
                 ),
               ],
             ),
-            const StringInput(
-              id: 'labelText',
-              uiSettings: StringInputUiSettings(
-                labelText: 'Titre',
-              ),
-            ),
-          ],
-        ),
-        const DynamicInputsNode(
-          id: 'availibleValues',
-          exportSettings: ExportSettings(type: ExportType.list),
-          templates: [
-            DynamicInputTemplate(
-              input: StringInput(
-                id: '', // will be overwritten
-                isRequired: true,
-                uiSettings: StringInputUiSettings(
-                  labelText: 'Valeur',
-                  autofocus: true,
+            const DynamicInputsNode(
+              id: 'availibleValues',
+              exportSettings: ExportSettings(type: ExportType.list),
+              templates: [
+                DynamicInputTemplate(
+                  input: StringInput(
+                    id: '', // will be overwritten
+                    isRequired: true,
+                    uiSettings: StringInputUiSettings(
+                      labelText: 'Valeur',
+                      autofocus: true,
+                    ),
+                  ),
                 ),
+              ],
+              uiSettings: DynamicInputsNodeUiSettings(
+                labelText: 'Ajouter une option',
               ),
             ),
           ],
-          uiSettings: DynamicInputsNodeUiSettings(
-            labelText: 'Ajouter une option',
-          ),
-        ),
-      ],
-      exportSettings: const ExportSettings(
-        metadata: {
-          'runtimeType': 'selectString',
-        },
-      ),
+        );
+      },
     );
