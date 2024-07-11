505b15a167e7af26eac8f30708af0350644a67be
SymAir <symeon@oclocher.fr>
2024-07-12 00:07:41 +0200

    Added temp submission

--- lib/src/builder/default_widget/inputs_node_expander.dart
+++ lib/src/builder/default_widget/inputs_node_expander.dart
@@ -16,53 +16,65 @@ class InputsNodeExpander extends StatelessWidget {
       helperText: data.uiSettings.helperText,
       errorText: data.errorText,
       trailing: const Icon(Icons.chevron_right),
-      onTap: () => context.pushPage(
-        RepositoryProvider.value(
-          value: context.read<RootNode>(),
-          child: MultiBlocProvider(
-            providers: [
-              BlocProvider.value(
-                value: context.read<WoFormValuesCubit>(),
-              ),
-              BlocProvider.value(
-                value: context.read<WoFormStatusCubit>(),
-              ),
-              BlocProvider.value(
-                value: context.read<WoFormLockCubit>(),
-              ),
-            ],
-            child: Scaffold(
-              appBar: AppBar(),
-              body: SingleChildScrollView(
-                child: SafeArea(
-                  child: Builder(
-                    builder: (context) {
-                      final fieldData = WoFieldData(
-                        path: data.path,
-                        input: data.input,
-                        value: null,
-                        errorText: null,
-                        uiSettings: data.uiSettings,
-                        onValueChanged: (_) {},
-                      );
+      onTap: () => pushPage(context),
+      shrinkWrap: false,
+    );
 
-                      return (data.uiSettings.widgetBuilder ??
-                              WoFormTheme.of(context)
-                                  ?.inputsNodeWidgetBuilder ??
-                              InputsNodeWidget.new)
-                          .call(fieldData);
-                    },
-                  ),
+    return (WoFormTheme.of(context)?.inputHeaderBuilder ?? InputHeader.new)
+        .call(headerData);
+  }
+
+  void pushPage(BuildContext context) {
+    final valuesCubit = context.read<WoFormValuesCubit>();
+    valuesCubit.setTemporarySubmitData(
+      onSubmitting: () async {
+        valuesCubit.clearTemporarySubmitData();
+        Navigator.pop(context);
+      },
+      path: data.path,
+    );
+
+    context.pushPage(
+      RepositoryProvider.value(
+        value: context.read<RootNode>(),
+        child: MultiBlocProvider(
+          providers: [
+            BlocProvider.value(
+              value: context.read<WoFormValuesCubit>(),
+            ),
+            BlocProvider.value(
+              value: context.read<WoFormStatusCubit>(),
+            ),
+            BlocProvider.value(
+              value: context.read<WoFormLockCubit>(),
+            ),
+          ],
+          child: Scaffold(
+            appBar: AppBar(),
+            body: SingleChildScrollView(
+              child: SafeArea(
+                child: Builder(
+                  builder: (context) {
+                    final fieldData = WoFieldData(
+                      path: data.path,
+                      input: data.input,
+                      value: null,
+                      errorText: null,
+                      uiSettings: data.uiSettings,
+                      onValueChanged: (_) {},
+                    );
+
+                    return (data.uiSettings.widgetBuilder ??
+                            WoFormTheme.of(context)?.inputsNodeWidgetBuilder ??
+                            InputsNodeWidget.new)
+                        .call(fieldData);
+                  },
                 ),
               ),
             ),
           ),
         ),
       ),
-      shrinkWrap: false,
     );
-
-    return (WoFormTheme.of(context)?.inputHeaderBuilder ?? InputHeader.new)
-        .call(headerData);
   }
 }
