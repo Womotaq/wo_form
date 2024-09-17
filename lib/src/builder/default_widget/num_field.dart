d246f769450d2a18193822fce53099a3f56232aa
SymAir <symeon@oclocher.fr>
2024-09-17 18:30:17 +0200

    Removed controller.dispose of NumField

--- lib/src/builder/default_widget/num_field.dart
+++ lib/src/builder/default_widget/num_field.dart
@@ -1,30 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:wo_form/wo_form.dart';
 
-class NumField extends StatefulWidget {
+class NumField extends StatelessWidget {
   const NumField(this.data, {super.key});
 
   final WoFieldData<NumInput, num?, NumInputUiSettings> data;
-
-  @override
-  State<NumField> createState() => _NumFieldState();
-}
-
-class _NumFieldState extends State<NumField> {
-  final countController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-
-    countController.text = widget.data.value?.toString() ?? '';
-  }
-
-  @override
-  void dispose() {
-    countController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,30 +17,32 @@ class _NumFieldState extends State<NumField> {
         right: 4,
       ),
       title: Text(
-        widget.data.uiSettings.labelText ?? '',
+        data.uiSettings.labelText ?? '',
         style: inputDecorationTheme.labelStyle,
       ),
-      subtitle: widget.data.errorText != null
+      subtitle: data.errorText != null
           ? Text(
-              widget.data.errorText!,
+              data.errorText!,
               style: inputDecorationTheme.errorStyle ??
                   theme.textTheme.labelMedium
                       ?.copyWith(color: theme.colorScheme.error),
             )
-          : (widget.data.uiSettings.helperText ?? '').isNotEmpty
+          : (data.uiSettings.helperText ?? '').isNotEmpty
               ? Text(
-                  widget.data.uiSettings.helperText ?? '',
+                  data.uiSettings.helperText ?? '',
                   style: inputDecorationTheme.helperStyle ??
                       theme.textTheme.labelMedium,
                 )
               : null,
       trailing: NumSelector.withTextController(
-        controller: countController,
-        onChanged: widget.data.onValueChanged,
+        controller: TextEditingController(
+          text: data.value?.toString() ?? '',
+        ),
+        onChanged: data.onValueChanged,
         axis: Axis.horizontal,
-        minCount: widget.data.input.minBound,
-        maxCount: widget.data.input.maxBound,
-        unit: widget.data.uiSettings.unit,
+        minCount: data.input.minBound,
+        maxCount: data.input.maxBound,
+        unit: data.uiSettings.unit,
       ),
     );
   }
