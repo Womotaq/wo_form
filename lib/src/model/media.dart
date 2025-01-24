b96732370136bdfac89ddba3385f65791cadc060
SymAir <symeon@oclocher.fr>
2025-01-24 11:31:56 +0100

    Added XFile.video

--- lib/src/model/media.dart
+++ lib/src/model/media.dart
@@ -44,9 +44,7 @@ sealed class Media with _$Media {
         MediaUrl() =>
           await context.read<MediaService>().typeOfMediaUrl(this as MediaUrl),
         MediaFile(file: final file) =>
-          file.mimeType?.startsWith('video') ?? false
-              ? MediaType.video
-              : MediaType.image,
+          file.isVideo ? MediaType.video : MediaType.image,
       };
 
   Future<MediaUrl> uploaded({
@@ -125,3 +123,25 @@ sealed class MediaImportMethod with _$MediaImportMethod {
 }
 
 enum MediaPickSource { gallery, camera }
+
+extension XFileX on XFile {
+  bool get isVideo {
+    if (mimeType?.contains('video') ?? false) return true;
+
+    final extension = name.split('.').lastOrNull;
+    if (extension == null) return false;
+
+    return [
+      'mp4',
+      'mkv',
+      'avi',
+      'mov',
+      'wmv',
+      'flv',
+      'webm',
+      '3gp',
+      'mpeg',
+      'ogv',
+    ].contains(extension);
+  }
+}
