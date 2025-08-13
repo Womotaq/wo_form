801502f143ffb71a5f6fd4234acaebcf3b69f546
SymAir <symeon@oclocher.fr>
2025-08-13 16:38:52 +0200

    Improved FutureNodeBuilder

--- lib/src/builder/future_node_builder.dart
+++ lib/src/builder/future_node_builder.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wo_form/wo_form.dart';
 
-class FutureNodeBuilder<T> extends StatelessWidget {
+class FutureNodeBuilder<T> extends StatefulWidget {
   const FutureNodeBuilder({
     required this.parentPath,
     required this.child,
@@ -12,12 +12,52 @@ class FutureNodeBuilder<T> extends StatelessWidget {
   final String parentPath;
   final FutureNode<T> child;
 
+  @override
+  State<FutureNodeBuilder<T>> createState() => _FutureNodeBuilderState<T>();
+}
+
+class _FutureNodeBuilderState<T> extends State<FutureNodeBuilder<T>> {
+  @override
+  void initState() {
+    super.initState();
+
+    final childPath = '${widget.parentPath}/${widget.child.id}';
+    getData(
+      (snapshot) => context.read<WoFormValuesCubit>().onValueChanged(
+        path: childPath,
+        value: snapshot,
+        updateStatus: UpdateStatus.ifPathAlreadyVisited,
+      ),
+    );
+  }
+
   Future<void> getData(
     void Function(AsyncSnapshot<T?> snapshot) onSnapshotChanged,
   ) async {
     try {
-      final data = await child.future;
-      onSnapshotChanged(AsyncSnapshot.withData(ConnectionState.done, data));
+      onSnapshotChanged(AsyncSnapshot.waiting());
+      final data = await widget.child.future;
+
+      if (!mounted) return;
+
+      final snapshot = AsyncSnapshot.withData(ConnectionState.done, data);
+      onSnapshotChanged(snapshot);
+
+      if (widget.child.willResetToInitialValues) {
+        final valuesCubit = context.read<WoFormValuesCubit>();
+
+        final newInitialValues = widget.child.getInitialValues(
+          parentPath: widget.parentPath,
+          initialSnapshot: snapshot,
+        );
+        for (final entry in newInitialValues.entries) {
+          valuesCubit.onValueChanged(
+            path: entry.key,
+            value: entry.value,
+            updateStatus: UpdateStatus.ifPathAlreadyVisited,
+          );
+        }
+      }
     } catch (error) {
       onSnapshotChanged(AsyncSnapshot.withError(ConnectionState.done, error));
     }
@@ -25,45 +65,18 @@ class FutureNodeBuilder<T> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final valuesCubit = context.read<WoFormValuesCubit>();
-
-    final childPath = '$parentPath/${child.id}';
-
-    getData(
-      (snapshot) => valuesCubit.onValueChanged(
-        path: childPath,
-        value: snapshot,
-        updateStatus: UpdateStatus.ifPathAlreadyVisited,
-      ),
-    );
+    final childPath = '${widget.parentPath}/${widget.child.id}';
 
     return WoFormValueBuilder<AsyncSnapshot<T?>>(
       path: childPath,
       builder: (context, snapshot) {
-        final input = child.builder(
+        final input = widget.child.builder(
           childPath,
           snapshot ?? const AsyncSnapshot.nothing(),
         );
 
-        if (child.willResetToInitialValues) {
-          if (snapshot?.connectionState == ConnectionState.done) {
-            final newInitialValues = child.getInitialValues(
-              parentPath: parentPath,
-              initialSnapshot: snapshot,
-            );
-
-            for (final entry in newInitialValues.entries) {
-              valuesCubit.onValueChanged(
-                path: entry.key,
-                value: entry.value,
-                updateStatus: UpdateStatus.ifPathAlreadyVisited,
-              );
-            }
-          }
-        }
-
         return input.toWidget(
-          key: ValueKey(snapshot?.connectionState),
+          key: Key(snapshot?.connectionState.name ?? 'none'),
           parentPath: childPath,
         );
       },
