e4d97ead276929e423033e5db4998021755b3ebf
chresyr <dev.chresyr@gmail.com>
2025-11-24 22:29:43 +0100

    Test

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:wo_form_example/app.dart';
-import 'package:wo_form_example/firebase_options.dart';
+import 'package:wo_form_example/firebase_optionsOTHER.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
