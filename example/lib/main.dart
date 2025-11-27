cb3e006c4919ad13a3aad150ce0ca2005db0d97d
chresyr <dev.chresyr@gmail.com>
2025-11-27 21:53:54 +0100

    Allowed debugging without firebase options

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,9 +6,15 @@ import 'package:wo_form_example/firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    // ignore: avoid_catching_errors
+  } on UnimplementedError catch (e) {
+    // ignore: avoid_print
+    print(e.message);
+  }
 
   runApp(const WoFormExamplesApp());
 }
