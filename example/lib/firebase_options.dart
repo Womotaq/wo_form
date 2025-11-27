8076062eeeaa71a18378dbaba80681c73c392288
chresyr <dev.chresyr@gmail.com>
2025-11-27 21:47:30 +0100

    Add firebase_options stub for CI

---
+++ example/lib/firebase_options.dart
@@ -0,0 +1,10 @@
+// example/lib/firebase_options.dart
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    throw UnimplementedError(
+      'Run flutterfire configure to generate real options.',
+    );
+  }
+}
